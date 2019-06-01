b57ac43333b25f82b506b4b21f93a4b76accba92
Sjoerd Thijsse <sjoerdthijsse@gmail.com>
2019-06-01 15:30:45 +0200

    feat: slock

---
+++ config.h
@@ -0,0 +1,15 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nobody";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "#141516",   /* after initialization */
+	[INPUT] =  "#efc0f1",   /* during input */
+	[FAILED] = "#db0a5b",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 5;
