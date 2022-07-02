e45238a2114afd7f81ea03b666aec70132d86c48
Sjoerd Thijsse <sjoerdthijsse@gmail.com>
2022-07-02 15:24:09 +0200

    feat: catppuccin

--- config.h
+++ config.h
@@ -3,9 +3,9 @@ static const char *user  = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "#191724",   /* after initialization */
-	[INPUT] =  "#e0def4",   /* during input */
-	[FAILED] = "#eb6f92",   /* wrong password */
+	[INIT] =   "#1E1E2E",   /* after initialization */
+	[INPUT] =  "#D9E0EE",   /* during input */
+	[FAILED] = "#96CDFB",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
